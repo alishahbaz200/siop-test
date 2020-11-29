61e0ac15fdd1822a042d0388643992d168df3b4a
alishahbaz <mralish.7.9@gmail.com>
2020-11-29 18:03:39 +0330

    create a checkout

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./collection-overview.style.scss"
+import {connect} from 'react-redux'
+import {createStructuredSelector} from "reselect";
+import CollectionPreview from "../collection-preview/collection-preview";
+import {selectCollection} from "../../redux/shop/shop.selectors";
+
+const CollectionOverview = ({collections}) => {
+    return (
+        <div className="collections-overview">
+            {collections && collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+        </div>
+    )
+}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollection
+})
+export default connect(mapStateToProps)(CollectionOverview)
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 import CartReducer from './cart/cart-reducer';
-
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userReducer from './user/user.reducer';
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
-  user: userReducer,
-  cart:CartReducer
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: CartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
